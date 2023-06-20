bffc8be96222692548fdb7109c47bd9cf165246c
ZKkkk <dxfdyxddd@126.com>
2023-06-20 14:22:58 +0800

    pref: 添加cdn打包优化

---
+++ src/typing.d.ts
@@ -0,0 +1,7 @@
+declare module 'rollup-plugin-external-globals'
+
+declare interface Window {
+  React: any
+  ReactDOM: any
+  antd: any
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    importToCDN({
+      modules: [
+        {
+          name: 'react',
+          var: 'React',
+          path: `umd/react.production.min.js`,
+        },
+        {
+          name: 'react-dom',
+          var: 'ReactDOM',
+          path: `umd/react-dom.production.min.js`,
+        },
+      ],
+    }),
+  ],
 })
